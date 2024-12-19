c5d48892be56d227e3591edcc90d29c88ad3b94a
FernandoTejedorL <fernandotejedorlopez@gmail.com>
2024-12-19 11:59:06 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import { BrowserRouter } from 'react-router-dom';
 import Router from './router/Router';
 import { GlobalStyles } from './styles/GlobalStyles';
-// import Header from './components/header/Header';
-// import { useState } from 'react';
 
 const App = () => {
-	// const [showMenu, setShowMenu] = useState(false);
 	return (
 		<BrowserRouter>
 			<GlobalStyles />
